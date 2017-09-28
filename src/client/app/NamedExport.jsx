30cad2c15e9c1ec460a5b79144ff435002ac971e
Jyothis Mathew <jyothismathewm@yahoo.com>
2017-09-28 02:02:03 -0400

    Named Export Component

---
+++ src/client/app/NamedExport.jsx
@@ -0,0 +1,19 @@
+import React,{Component} from 'react';
+
+export class Header extends Component {
+    render(){
+        return(
+            <div> Header named (no default)</div>
+        );
+
+    }
+}
+
+export class Footer extends Component{
+    render(){
+        return(
+            <div> Footer named (no default)</div>
+        );
+
+    }
+}
--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,15 +3,18 @@ import {render} from 'react-dom';
 import AwesomeComponent from './AwesomeComponent.jsx';
 import Shoppinglist from './Shoppinglist.jsx';
 import Name from './FunctionComponent.jsx'
+import {Header,Footer} from './NamedExport.jsx'
 
 class App extends Component {
     render () {
         return (
             <div>
+                <Header />
                 <p> Hello React!</p>
                 <AwesomeComponent />
                  <Shoppinglist />
                 <Name name='yes it is working' />
+                <Footer />
             </div>
         );
     }
